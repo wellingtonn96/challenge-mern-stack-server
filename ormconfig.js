684bb34df0c3b37b7805bbf3624dccb1facbfc8a
Wellington Santos <weltossousa@gmail.com>
2020-12-02 10:18:07 -0300

    subindo o projeto

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": ["./dist/src/models/*.js"],
+  "migrations": ["./dist/src/database/migrations/*.js"],
+  "cli": {
+    "migrationsDir": "./src/database/migrations"
+  }
+}
